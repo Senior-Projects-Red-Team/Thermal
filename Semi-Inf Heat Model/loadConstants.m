045563e35aaa242c52f6fd75ee06e9fa4c3b876c
jasa1519 <jasa1519@colorado.edu>
2025-09-18 16:02:15 -0600

    Updates with better constants, and slight tweaks to the model.
    Incorporated better information from power and structures team, as well as minor tweaks to the model  for greater clarity and fidelity.

--- Semi-Inf Heat Model/loadConstants.m
+++ Semi-Inf Heat Model/loadConstants.m
@@ -10,6 +10,9 @@ constants.regolith.diffusivity = constants.regolith.conductivity / (constants.re
 constants.regolith.diffusivity_alt = 2e-7; % m^2/s
 constants.regolith.conductivity_alt = constants.regolith.diffusivity_alt*constants.regolith.density*constants.regolith.cp;% W/m*K
 
+constants.power.battery_mass = 0;
+constants.power.battery_cp = 0.8; % kj/kgK 
+
 constants.greenhouse.max_temp = 28 + 273.15; % K
 constants.greenhouse.min_temp = 22 + 273.15; % K
 constants.greenhouse.atm_cp = 1.005; % kJ/kgK % Assuming similar to normal air for now
@@ -18,7 +21,7 @@ constants.greenhouse.water_cp = 4.18; % kJ/kgK
 constants.greenhouse.atm_density = 1.204; % kg/m^3 Assumes normal air
 constants.greenhouse.water_density = 1000; %kg/m^3
 constants.greenhouse.water_mass = 1; % I'm temporarily assuming 1 kg of water, more specific values needed.
-constants.greenhouse.structure_mass = 10; % Consult with SE and Structures Teams
+constants.greenhouse.structure_mass = 6; % Consult with SE and Structures Teams
 constants.greenhouse.AreaMin = 2*0.2*0.2 + 4*0.2*0.1; % 20cm tall, 20cm long, 10cm wide %% Final answer in m^2
 constants.greenhouse.AreaMax = 2*0.5*0.5 + 4*0.5*0.25; % 50 cm, 50cm, 25cm %% Final answer in m^2
 constants.greenhouse.VolMin = 0.2*0.2*0.1; % m^3
@@ -30,7 +33,7 @@ constants.greenhouse.VolWorking = 0.35^3;
 constants.greenhouse.HeatMin = determineinternalHeat(constants.greenhouse.VolWorking, constants.greenhouse.min_temp, constants);
 constants.greenhouse.HeatMax = determineinternalHeat(constants.greenhouse.VolWorking, constants.greenhouse.max_temp, constants);
 
-constants.greenhouse.lightWatts = 64; % Watts. How much power the lights are using, current estimate is based on 2 32 Watt bulbs.
+constants.greenhouse.lightWatts = 32; % Watts. How much power the lights are using, current estimate is based on 2 32 Watt bulbs.
 
 constants.greenhouse.HeatGen = constants.greenhouse.lightWatts;
 
--- Semi-Inf Heat Model/main.m
+++ Semi-Inf Heat Model/main.m
@@ -9,21 +9,41 @@ clc;
 tic();
 constants = loadConstants();
 
+
+
+
+
+%% Temp of external surface
+
+Q_dot = constants.greenhouse.HeatGen; % Here we're assuming that we'll maintain the internal temperature.
+
+semiInfModel(32, constants); % Low end
+semiInfModel(50, constants); % Theoretical maximum given by Power Team
+semiInfModel(64, constants);
+semiInfModel(80, constants);
+%semiInfModel(96, constants);
+%semiInfModel(128, constants); % Approximates Earth Sunlight.
+
+toc()
+
+function semiInfModel(Q_dot, constants)
+
 heat_flux = @(k, T_s, T_init, alpha, t) (k.*(T_s-T_init))./((3.1415.*alpha.*t).^0.5);
 
 delta_t = 300; % s
 numSteps = (15*24*60*60)/delta_t + 1;
 time_vect = transpose(linspace(0,15*24*60*60,numSteps));
 
-%% Temp of external surface
-
-Q_dot = constants.greenhouse.HeatGen; % Here we're assuming that we'll maintain the internal temperature.
-
 temp_trials = 500;
 
 regolith_temp = 120; % Kelvin
 
 Surface_temps = linspace(regolith_temp,constants.greenhouse.max_temp, temp_trials);
+
+R_trials = 100;
+
+% R_range = linspace(0,3,R_trials);
+% Temps_from_R = constants.greenhouse.max_temp-(R_range.*Q_dot);
 
 R_vals = (constants.greenhouse.max_temp - Surface_temps)/Q_dot;
 
@@ -54,7 +74,7 @@ for i = 0:9
 end
 yline(Q_dot/1000);
 legend(num2str(transpose(legend_arr)))
-title("Heat Flow through Regolith with specified surface Temps")
+title("Heat Flow through Regolith with specified surface Temps and " + Q_dot+ " (W) heat gen")
 ylabel("Heat Flow (kW)")
 xlabel("Time (Days)")
 hold off
@@ -67,7 +87,7 @@ for i = 0:9
 end
 legend(num2str(transpose(legend_arr)))
 yline(0);
-title("Net Greenhouse Energy with specified surface Temps")
+title("Net Greenhouse Energy with specified surface Temps and " + Q_dot+ " (W) heat gen")
 ylabel("Net Internal Energy (kJ)")
 xlabel("Time (Days)")
 hold off
@@ -80,7 +100,7 @@ for i = 0:9
 end
 yline(Q_dot/1000);
 legend(num2str(transpose(legend_arr)))
-title("Heat Flow through Regolith with specified Heat Resistance")
+title("Heat Flow through Regolith with specified Heat Resistance and " + Q_dot+ " (W) heat gen")
 ylabel("Heat Flow (kW)")
 xlabel("Time (Days)")
 hold off
@@ -93,7 +113,7 @@ for i = 0:9
 end
 yline(constants.greenhouse.HeatMin,LineStyle="--");
 yline(constants.greenhouse.HeatMax,LineStyle="--");
-title("Net Greenhouse Energy with specified Heat Resistance")
+title("Net Greenhouse Energy with specified Heat Resistance and "  + Q_dot+ " (W) heat gen")
 legend(num2str(transpose(legend_arr)))
 ylabel("Total Internal Energy (kJ)")
 xlabel("Time (Days)")
@@ -101,23 +121,22 @@ hold off
 
 %% Finding target values using this
 
-R_targ = 1.7; % Inspection of the graph reveals that this will allow 0 heaters.
+% R_targ = 4; % Inspection of the graph reveals that this will allow 0 heaters.
+% 
+% ks = linspace(0.001, 0.1, 1001); % A Range of somewhat reasonable k values
+% 
+% Ls = R_targ*ks*constants.greenhouse.AreaWorking;
+% 
+% figure()
+% hold on
+% title("Wall thickness with Thermal Conductivities to hit R_{targ}")
+% plot(ks, Ls)
+% xline(0.005)
+% xline(0.02)
+% xline(0.04)
+% legend("Thickness vs k", "Vacuum Insulation Panels", "Urethane Foam", "Mineral Wools")
+% xlabel("Thermal Conductivity (W/mK)")
+% ylabel("")
+% hold off
 
-ks = linspace(0.001, 0.1, 1001); % A Range of somewhat reasonable k values
-
-Ls = R_targ*ks*constants.greenhouse.AreaWorking;
-
-figure()
-hold on
-title("Wall thickness with Thermal Conductivities to hit R_targ")
-plot(ks, Ls)
-xline(0.005)
-xline(0.02)
-xline(0.04)
-legend("Thickness vs k", "Vacuum Insulation Panels", "Urethane Foam", "Mineral Wools")
-xlabel("Thermal Conductivity (W/mK)")
-ylabel("")
-hold off
-
-toc()
-
+end
